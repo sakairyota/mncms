5bd9c413c5b779823e4e4ca9d088dc6a7860ada7
sakairyota <sakai@cloudstudy.com>
2011-09-19 00:20:25 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mncms::Application.config.secret_token = '20b55c2ed265f6a450f183b6405ae9dbedae8f4b97ea16759f237642aa37637bf8614050096992a2e0874d0ce2cea4c54e4f36d5f43fdd582c81caee2a49d608'
